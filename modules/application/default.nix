667feb124c36fe5527acbb597fb92acfca09bd92
enqack <enqack@gmail.com>
2024-03-16 03:49:37 +0000

    Added some application modules.

--- modules/application/default.nix
+++ modules/application/default.nix
@@ -1,15 +1,29 @@
+{lib, ...}:
+
+with lib;
 {
   imports = [
-    ./bat.nix
+    ./bat.nix 
+    ./bind.nix
+    ./binutils.nix
+    ./coreutils.nix
     ./curl.nix
     ./dhcpcd.nix
+    ./dust.nix
+    ./e2fsprogs.nix
+    ./fzf.nix
     ./git.nix
     ./glow.nix
     ./gpm.nix
     ./htop.nix
+    ./iftop.nix
+    ./iotop.nix
+    ./less.nix
+    ./lsof.nix
+    ./pciutils.nix
     ./rsync.nix
-    ./tmux.nix
     ./tree.nix
+    ./tmux.nix
     ./vim.nix
     ./wget.nix
     ./zsh
