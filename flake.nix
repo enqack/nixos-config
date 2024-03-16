58d7f3c36878d2d746d90e86f7f4fc83dd873890
enqack <enqack@gmail.com>
2024-03-16 01:01:18 +0000

    Set nix versions to 23.11.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "The entry-point flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
