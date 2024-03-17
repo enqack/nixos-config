5eed16e9b40d745f8c579878886cb588b319d8d5
enqack <enqack@gmail.com>
2024-03-17 02:01:38 +0000

    Added some feature modules.

--- modules/feature/default.nix
+++ modules/feature/default.nix
@@ -1,8 +1,12 @@
 {
   imports = [
     ./boot
+    ./cross-compilation.nix
+    ./fonts.nix
     ./graphics
     ./home-manager.nix
     ./virtualization
+    ./s3ql.nix
+    ./secrets.nix    
   ];
 }
