2b0de33b4f8b317250129ed533b3c83a0921edd4
enqack <enqack@gmail.com>
2024-11-12 12:32:00 -0500

    LVM mount attempt v10.

--- config/filesystem/lvm.nix
+++ config/filesystem/lvm.nix
@@ -11,8 +11,8 @@
 
   boot.initrd.systemd.services.lvm-activate = {
     description = "Activate LVM Pools";
-    wants = [ "basic.target" ];
-    before = [ "basic.target" ];
+    wants = [ "local-fs-pre.target" ];
+    before = [ "local-fs-pre.target" ];
     serviceConfig = {
       ExecStartPre = "${pkgs.kmod}/bin/modprobe dm_mod";
       ExecStart = "${pkgs.lvm2}/bin/lvm vgchange -ay";
