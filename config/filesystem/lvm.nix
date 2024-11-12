d310761e6a070d9e1273403b2158fcae0e0126a7
enqack <enqack@gmail.com>
2024-11-12 11:45:50 -0500

    LVM mount attempt v8.

--- config/filesystem/lvm.nix
+++ config/filesystem/lvm.nix
@@ -9,14 +9,13 @@
   services.lvm.enable = true;
   boot.initrd.services.lvm.enable = true;
 
-  boot.initrd.systemd.units."lvm-activate.service" = {
+  boot.initrd.systemd.services.lvm-activate = {
     description = "Activate LVM Pools";
     wants = [ "initrd-root-fs.target" ];
     before = [ "initrd-root-fs.target" ];
     serviceConfig = {
-      ExecStartPre = [ "${pkgs.kmod}/bin/modprobe dm_mod" ];
-      ExecStart = [ "${pkgs.lvm2}/bin/lvm vgchange -ay" ];
+      ExecStartPre = "${pkgs.kmod}/bin/modprobe dm_mod";
+      ExecStart = "${pkgs.lvm2}/bin/lvm vgchange -ay";
     };
   };
-
 }
