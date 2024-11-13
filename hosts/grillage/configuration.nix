47ff0bafe45681e38fd80f714271a4bdaaf9ad86
enqack <enqack@gmail.com>
2024-11-13 01:10:47 -0500

    Grillage tweek.

--- hosts/grillage/configuration.nix
+++ hosts/grillage/configuration.nix
@@ -17,7 +17,7 @@
   systemd.network.networks."10-enp" = {
     matchConfig.Name = "enp*";
     networkConfig = {
-      DHCP = lib.mkDefault "no";
+      DHCP = lib.mkForce "no";
       Address = "192.168.10.6/24";
       Gateway = "192.168.10.1";
       DNS = [ "192.168.10.1" "1.1.1.1" "8.8.8.8" ];
