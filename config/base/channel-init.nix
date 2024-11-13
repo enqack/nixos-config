311deb8d2efcd243c08dd64d67876fc76d5c37aa
enqack <enqack@gmail.com>
2024-11-13 12:59:27 -0500

    Modified channel init activation script.

--- config/base/channel-init.nix
+++ config/base/channel-init.nix
@@ -4,7 +4,7 @@
   # Define an activation script to run once per machine
   system.activationScripts.channelInit = {
     text = ''
-      nix-channel --add https://nixos.org/channels/nixos-${system.stateVersion}} nixpkgs
+      nix-channel --add https://nixos.org/channels/nixos-${config.system.stateVersion}} nixpkgs
     '';
   };    
 }
