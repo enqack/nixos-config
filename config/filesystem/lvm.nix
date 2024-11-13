9e33c74a7cc3778b24b40886472b8662fa22e285
enqack <enqack@gmail.com>
2024-11-12 19:16:29 -0500

    LVM mount attempt v24.

--- config/filesystem/lvm.nix
+++ config/filesystem/lvm.nix
@@ -11,5 +11,18 @@
   # boot.initrd.systemd.extraBin = {
   #   lvm = "${pkgs.lvm2}/bin/lvm";
   # };
-  boot.kernelParams = [ "systemd.log_level=debug" "systemd.log_target=console" ];
+  #boot.kernelParams = [ "systemd.log_level=debug" "systemd.log_target=console" ];
+
+  systemd.services.lvm2-lvmetad = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    after = [ "systemd-udevd.service" "systemd-journald.service" ];
+  };
+
+  systemd.services.lvm2-activation = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    after = [ "lvm2-lvmetad.service" ];
+    requires = [ "lvm2-lvmetad.service" ];
+  };
 }
