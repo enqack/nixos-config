4f1869647e38d661aff84a8332758778586208b1
enqack <enqack@gmail.com>
2024-11-13 00:01:12 -0500

    Changed grillage network configuration.

--- hosts/grillage/configuration.nix
+++ hosts/grillage/configuration.nix
@@ -17,6 +17,7 @@
   systemd.network.networks."10-enp" = {
     matchConfig.Name = "enp*";
     networkConfig = {
+      DHCP = "no";
       Address = "192.168.10.6/24";
       Gateway = "192.168.10.1";
       DNS = [ "192.168.10.1" "1.1.1.1" "8.8.8.8" ];
