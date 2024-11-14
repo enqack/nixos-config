4bef9b709fc3eb2043285a3f8c502baf1cc87ed3
enqack <enqack@gmail.com>
2024-11-14 13:46:43 -0500

    Made flake.nix host definitions dynamic.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,17 @@
 {
-  description = "NixOS configuration with Home Manager";
+  description = "NixOS configuration for NestOps";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
 
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
+    
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -19,99 +19,48 @@
   outputs = { self, nixpkgs, nixpkgs-unstable, disko, home-manager, sops-nix }:
   let
     system = "x86_64-linux";
-  in
-  {
-    nixosConfigurations = {
-      
-      reactor = nixpkgs.lib.nixosSystem {
+    lib = nixpkgs.lib;
+
+    # List of host definitions with extra modules
+    hosts = [
+      { name = "reactor"; extraModules = []; }
+      { name = "flex"; extraModules = []; }
+      { name = "nixany"; extraModules = []; }
+      { name = "nesttest"; extraModules = []; }
+      { name = "knell"; extraModules = []; }
+      { name = "grillage"; extraModules = [ sops-nix.nixosModules.sops ]; }
+    ];
+
+    # Function to find the index of an element by name
+    findIndex = name: list:
+      let
+        names = map (h: h.name) list;
+      in
+        builtins.elemAt (builtins.filter (i: builtins.elemAt names i == name) (lib.range 0 (builtins.length names - 1))) 0;
+
+
+    # Function to generate each host configuration
+    mkHost = host: lib.nixosSystem {
+      inherit system;
+
+      pkgs = import nixpkgs {
         inherit system;
-
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          ./hosts/reactor/configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
+        config.allowUnfree = true;
       };
 
-      flex = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          ./hosts/flex/configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-
-      nixany = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          disko.nixosModules.disko
-          ./hosts/nixany/configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-
-      nesttest = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          disko.nixosModules.disko
-          ./hosts/nesttest/configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-
-      knell = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          disko.nixosModules.disko
-          ./hosts/knell/configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-
-      grillage = nixpkgs.lib.nixosSystem {
-        inherit system;
-
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-        modules = [
-          disko.nixosModules.disko
-          ./hosts/grillage/configuration.nix
-          home-manager.nixosModules.home-manager
-        ];
-      };
-
+      modules = [
+        disko.nixosModules.disko
+        (./hosts/${host.name}/configuration.nix)
+        home-manager.nixosModules.home-manager
+      ] ++ host.extraModules;
     };
+
+  in {
+    # Generate host configurations from `hosts` list
+    nixosConfigurations = lib.genAttrs 
+      (map (h: h.name) hosts) 
+      (hostName: mkHost 
+        (builtins.elemAt hosts (findIndex hostName hosts))
+      );
   };
 }
-
