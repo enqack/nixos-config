dd2c4fd42889a32ecced037a31c0486f37f63b8d
enqack <enqack@gmail.com>
2026-08-12 11:50:03 -0400

    Fixed auto-upgrade module to use proper host config.

--- modules/linux/system/auto-upgrade/default.nix
+++ modules/linux/system/auto-upgrade/default.nix
@@ -1,20 +1,44 @@
 { config, lib, pkgs, ... }:
+
 let
   cfg = config.modules.system.auto-upgrade;
 in
 {
   options.modules.system.auto-upgrade = {
     enable = lib.mkEnableOption "system auto-upgrade configuration";
+
+    flake = lib.mkOption {
+      # Allow either a string (URL) or a path (local file location)
+      type = with lib.types; either str path;
+      default = "/etc/nixos";
+      description = "The flake URI (URL) or local file path to use for auto-upgrades.";
+      example = "github:user/repo"; 
+    };
+
+    dates = lib.mkOption {
+      type = lib.types.str;
+      default = "weekly";
+      description = "How often or when to run the auto-upgrade. Accepts systemd calendar format.";
+      example = "04:00";
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    # System auto upgrade
     system.autoUpgrade = {
       enable = true;
       allowReboot = false;
-      dates = "daily";
-      flake = "github:enqack/nixos-config#catalyst";
-      flags = [ "--update-input" "nixpkgs" "--commit-lock-file" ];
+      
+      dates = cfg.dates;
+
+      flake = toString cfg.flake;
+      
+      flags = [ 
+        "--update-input" 
+        "nixpkgs" 
+        "--commit-lock-file" 
+      ];
+      
+      randomizedDelaySec = "45min";
     };
   };
 }
